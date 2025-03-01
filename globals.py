481518663dba0c85ec0eff3740238a9a6935669d
john <john-a-harper@hotmail.com>
2025-03-01 15:22:36 +0100

    add set() function to globals

--- globals.py
+++ globals.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 from params import *
-
+from typing import Any
 
 class Globals:
 
@@ -11,3 +11,7 @@ class Globals:
     @staticmethod
     def init():
         Globals.speed = Params.get('default_speed')
+
+    @staticmethod
+    def set(name: str, value: Any) -> None:
+        setattr(Globals, name, value)
